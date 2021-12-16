3a272071385ed1a82cff5abfe95abc393da60a0c
Dan Loewenherz <dan@lionheartsw.com>
2021-12-15 22:06:23 -0600

    fix tests

--- day01/part1_test.go
+++ day01/part1_test.go
@@ -14,10 +14,10 @@ type Part1TestCase struct {
 func TestPart1CountIncreases(t *testing.T) {
 	var tests = []Part1TestCase{
 		{[]int{200}, 0},
-		{utils.IntegerLinesFromFile("part1_example_input.txt"), 7},
-		{utils.IntegerLinesFromFile("part1_input.txt"), 1374},
-		{utils.IntegerLinesFromFile("part2_example_output.txt"), 5},
-		{utils.IntegerLinesFromFile("part2_output.txt"), 1418},
+		{utils.IntegerLinesFromFile("testdata/part1_example_input.txt"), 7},
+		{utils.IntegerLinesFromFile("testdata/part1_input.txt"), 1374},
+		{utils.IntegerLinesFromFile("testdata/part2_example_output.txt"), 5},
+		{utils.IntegerLinesFromFile("testdata/part2_output.txt"), 1418},
 	}
 
 	for _, tt := range tests {
